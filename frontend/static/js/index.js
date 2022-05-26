2801a7248d7d6db0d897bc0ee8fc682cc9c6f833
tahirdibirovresearch <dibirovtahir@gmail.com>
2022-05-26 20:58:41 +0400

    Matches

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,23 @@
 const router = async ()=>{
-    
+    const routes = [
+        {path: "/", view: ()=>{console.log("Viewing Dashboard")}},
+        {path: "/posts", posts: ()=>{console.log("Viewing Posts")}},
+        {path: "/settings", settings: ()=>{console.log("Viewing Settings")}}
+    ]
+
+
+
+    const potentialMatches = routes.map(route =>{
+        return{
+            route: route,
+            isMatch: location.pathname === route.path  
+        }
+    })
+
+
+    console.log(potentialMatches)
+
 }
+
+
+router()
